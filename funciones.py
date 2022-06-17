f427340d6313b7a9d151c832026be10606cb640c
Cesar <ipog71@gmail.com>
2022-06-17 18:55:36 -0500

    Readme

--- funciones.py
+++
@@ -1,8 +0,0 @@
-#Funciones
-def calcularPromedio (alumno1, alumno2, alumno3):
-    suma = alumno1 + alumno2 + alumno3
-    promedio = suma/3
-    return promedio
-
-promedio = calcularPromedio(9,8,6)
-print("El promedio es: ", promedio)
