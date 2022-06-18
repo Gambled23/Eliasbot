5d813f6fd5e025c00538bf91f5730440c5247c0f
Cesar <ipog71@gmail.com>
2022-06-17 19:04:01 -0500

    Borrada basura

--- bot.py
+++ bot.py
@@ -39,14 +39,6 @@ async def printConsoleMessage(event):
     elif(event.content == 'pendejo' or event.content == 'baboso'):
         await event.message.respond('<@567039496533573632> ahi te hablan')
 
-# @bot.listen(hikari.GuildMessageCreateEvent)
-# async def printConsoleMessage(event):
-#    if(event.author.id == 320649011000246272):
-#         await event.message.respond('Cesar envio el mensaje')
-#    elif(event.author.id != 809479840444186654):
-#         await event.message.respond('Alguien mas envio el mensaje')
-
-
 # Comandos
 @bot.command
 @lightbulb.command('insulto', 'Dice un insulto racial!')
@@ -54,20 +46,12 @@ async def printConsoleMessage(event):
 async def insultoCDO(ctx):
     await ctx.respond(random.choice(respuestas.insulto))
 
-
 @bot.command
 @lightbulb.command('lol', 'Quieres jugar lol?')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def lolCDO(ctx):
     mensaje = random.choice(respuestas.lol)
     await ctx.respond(f'{mensaje} \n<@&810955574488465420>')
-
-#Crashea por alguna razon!!
-#@bot.command
-#@lightbulb.command('changelog', 'Ver los registros de cambio')
-#@lightbulb.implements(lightbulb.SlashCommand)
-#async def changelogCDO(ctx):
-#    await ctx.respond(respuestas.cambios)
 
 @bot.command
 @lightbulb.command('auxilio', 'Solo usalo en una emergencia si necesitas hablar con alguien')
