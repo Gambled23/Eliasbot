6005c4405993e7089d215d250bf7e72a3ad18a61
Cesar <ipog71@gmail.com>
2022-07-02 17:28:30 -0500

    boys dont cry update

--- bot.py
+++ bot.py
@@ -46,8 +46,61 @@ async def printConsoleMessage(event):
             mensajeFinal = random.choice(['Sí', 'No', 'Quizás'])
         case 'chinga tu madre'|'ctm'|'chinga tu madre pues':
             mensajeFinal = 'la tuya en vinagre'
-        case 'pruebota':
-            mensajeFinal = 'Mira soy un test'
+        case 'que bonita estás':
+            mensajeFinal = 'ok simp'
+        case 'que bonita estas':
+            mensajeFinal = 'No le sabes a los acentos ' + event.author.username
+        case '#p boys dont cry'|'#p the cure boys dont cry'|'#p boys dont cry the cure':
+            mensajeFinal = 'tas bien bro?'
+        case 'un baropapas':
+            mensajeFinal = random.choice(['Letsfuckingooooo', 'pensé q nunca lo dirías', 'BAROPAPASALAVERGA'])
+        case 'ramiro':
+            mensajeFinal = 'que chingue su madre ese puto viejo de mierda a la verga pinche señor inútil le voy a clavar un cuchillo en la puta garganta mientras me tomo su sangre y me masturbo en el agujero que le quedó del navajazo'
+        case 'luis':
+            mensajeFinal = 'chinga tu madre no me digas luis'
+        case 'Roman'|'roman'|'Román'|'román':
+            mensajeFinal = 'andele pendeja'
+        case 'Eliasbot podemos jugar lol?'|'eliasbot podemos jugar lol?':
+            mensajeFinal = random.choice(['sí','no'])
+        case '69'|'sesenta y nueve':
+            mensajeFinal = random.choice(respuestas.seisnueve)
+        case '420'|'4:20'|'cuatro veinte':
+            mensajeFinal = random.choice(respuestas.cuatroveinte)
+        case 'skyrim':
+            mensajeFinal = 'puto todd'
+        case 'ando bien pedo'|'ando bien pacheco'|'extraño a mi ex'|'ando bien grifo'|'ando bien drogado'|'ando medio pedo':
+            mensajeFinal = 'ya duermete wey al chile'
+        case "I would say I'm sorry"|'i would say im sorry':
+            if (event.author.id != 809479840444186654):
+                mensajeFinal = 'If I thought that it would change your mind'
+        case 'If I thought that it would change your mind'|'if I thought that it would change your mind':
+            if (event.author.id != 809479840444186654):
+                mensajeFinal = 'But I know that this time'
+        case 'But I know that this time'|'but I know that this time':
+            if (event.author.id != 809479840444186654):
+                mensajeFinal = 'I have said too much'
+        case 'I have said too much'|'i have said too much':
+            if (event.author.id != 809479840444186654):
+                mensajeFinal = 'Been too unkind'
+        case 'Been too unkind'|'been too unkind':
+            if (event.author.id != 809479840444186654):
+                mensajeFinal = 'I tried to laugh about it'
+        case 'I tried to laugh about it'|'i tried to laugh about it':
+            if (event.author.id != 809479840444186654):
+                mensajeFinal = 'Cover it all up with lies'
+        case 'Cover it all up with lies'|'cover it all up with lies':
+            if (event.author.id != 809479840444186654):
+                mensajeFinal = 'Hiding the tears in my eyes'
+        case 'Hiding the tears in my eyes'|'hiding the tears in my eyes':
+            if (event.author.id != 809479840444186654):
+                mensajeFinal = "Cause boys don't cry"
+        case "Cause boys don't cry"|"cause boys don't cry"|"Cause boys dont cry"|"cause boys dont cry":
+            if (event.author.id != 809479840444186654):
+                mensajeFinal = "Booooooys don't cry"
+        case "boys dont cry"|"boys don't cry"|"Boys don't cry"|"Boys dont cry":
+            if (event.author.id != 809479840444186654):
+                mensajeFinal = "BOOOOOYS DON'T CRYYY"
+                
     await event.message.respond(mensajeFinal)
 
 # Comandos
