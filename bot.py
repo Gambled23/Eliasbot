907e7083b724937f8423d7fbb4fb8c9310cb38ac
Cesar <ipog71@gmail.com>
2022-07-14 13:40:25 -0500

    .

--- bot.py
+++ bot.py
@@ -9,21 +9,22 @@ bot = lightbulb.BotApp(token='ODA5NDc5ODQwNDQ0MTg2NjU0.YCVs2Q.yODObIjLuocQuQxIGM
                        
 bot.load_extensions_from('./extensions')
 
-# Eventos
 @bot.listen(hikari.StartedEvent)
 async def botStarted(event):
-    print("Bot has been started")
+    print("Eliasbot ha iniciado correctamente, bienvenido, Gambled")
     
-#@bot.command
-#@lightbulb.command('testeo','comando de prueba')
-#@lightbulb.implements(lightbulb.SlashCommand)
-#async def testeo(ctx):
-#    em = hikari.Embed(title="Elias dice:", color=0x32441C)
-#    em.add_field(
-#        name=random.choice(respuestas.elias),
-#        value="-Elias",
-#        inline=False,
-#    )
-#    await ctx.respond(em)
+'''
+@bot.command
+@lightbulb.command('testeo','comando de prueba')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def testeo(ctx):
+    em = hikari.Embed(title="Elias dice:", color=0x32441C)
+    em.add_field(
+        name=random.choice(respuestas.elias),
+        value="-Elias",
+        inline=False,
+    )
+    await ctx.respond(em)
+'''
 
 bot.run()
