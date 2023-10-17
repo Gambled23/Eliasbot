9cdde56fd6766e5c00aecd7457f8f6d577436419
Gambled23 <ipog71@gmail.com>
2023-10-17 10:30:30 -0600

    soy pendejo

--- autoDeploy.sh
+++ autoDeploy.sh
@@ -3,7 +3,7 @@
 cd /home/cesar/eliasbot
 source venv/bin/activate
 
-while true do
+while true; do
     screen -dmS eliasbot python3 bot.py
     sleep $((24 * 60 * 60))
     screen -X -S eliasbot quit
